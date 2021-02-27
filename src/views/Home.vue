33a17d8029cdea6db84b79f880574761dffec76d
keisuke20000923@gmail.com <hondsoccer0413@gmail.com>
2021-02-27 15:52:02 +0900

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <h1>aaaa</h1>
+    <form action="******" method="post" enctype="multipart/form-data">
+
+    <div id="upload" class="form-group commonStyle" v-bind:class="{'styleA':styleA, 'styleB':styleB}" @dragover.prevent="changeStyle($event,'ok')" @dragleave.prevent="changeStyle($event,'no')" @drop.prevent="uploadFile($event)">
+
+        <label for="upload_image" class="button">
+        <p>画像を選択</p>
+        <input id="upload_image" type="file" name="img" @change="uploadFile($event)" style="display:none;" accept="image/*">
+        </label>
+
+        <!-- ここからプレビュー機能の部分 -->
+        <p>またはここに画像ファイルをドラッグ＆ドロップ</p>
+        <img v-show="preview" v-bind:src="preview" style="width:300px;">
+        <p v-show="preview"> {{name}} </p>
+        <!-- ここまでプレビュー機能の部分 -->
+
+    </div>
+        <button class="btn btn-success" style="width:400px; margin-left:30px;">アップロード</button>
+
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    preview:'',
+    name:'',
+    styleA:true,
+    styleB:false,
+  }),
+  methods: {
+        uploadFile: function(event){
+            this.styleA = true;
+            this.styleB = false;
+            const files = event.target.files ? event.target.files : event.dataTransfer.files;
+            const file = files[0];
+            const reader = new FileReader();
+            reader.onload = event => {
+                this.preview = event.target.result;
+            };
+            reader.readAsDataURL(file);
+            this.name = files[0].name;
+            document.getElementById("upload_image").files = files;
+        },
+        changeStyle: function(event,flag){
+            if(flag=='ok'){
+                this.styleA = false;
+                this.styleB = true;
+            }else{
+                this.styleA = true;
+                this.styleB = false;
+            }
+        },
+    },
 }
 </script>
+
+<style scoped>
+  .commonStyle{ padding:30px; text-align:center; margin:30px; width:400px;}
+  .styleA{border:3px dotted gray;}
+  .styleB{border:3px dotted rgba(0,200,0,0.7);}
+  .button{border:1px solid green; padding:3px; border-radius:5px; background-color:white;}
+  .button p {color:green; margin-top:10px; margin-left:10px; margin-right: 10px;}
+</style>
